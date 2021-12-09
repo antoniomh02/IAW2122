d2c7697a78f39f07dcc1dc60111ce5062c948287
antoniomh02 <a_antmayhur@iestriana.com>
2021-12-09 18:37:34 +0100

    poo01

--- POO01/src/es/iestriana/poo01/datos/Principal.java
+++ POO01/src/es/iestriana/poo01/datos/Principal.java
@@ -4,12 +4,13 @@ public class Principal {
 
 	public static void main(String[] args) {
 		Cuenta c1 = new Cuenta();
-		Cuenta c2 = new Cuenta("Mi nombre", "6732549543287", 2000000, 3.5);
+		Cuenta c2 = new Cuenta("Mi nimbre", "6666565775", 2000000, 3.5);
 		
-		c1.setNombre("Otro nombre");
-		c1.setCcc("43876598743659843");
+		c1.setNombre("otro nombre");
+		c1.setCcc("576353763");
 		c1.setSaldo(3000000);
 		c1.setTipoDeInteres(2.3);
+		
 		
 		c1.ingresar(10000);
 		c2.reintegro(3000);
