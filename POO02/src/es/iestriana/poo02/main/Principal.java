3fe221e790d35d52ba2a28c018ce33a35e29c19d
antoniomh02 <a_antmayhur@iestriana.com>
2021-12-09 19:36:54 +0100

    poo02

--- POO02/src/es/iestriana/poo02/main/Principal.java
+++ POO02/src/es/iestriana/poo02/main/Principal.java
@@ -2,6 +2,7 @@ package es.iestriana.poo02.main;
 
 import es.iestriana.poo02.datos.Circulo;
 import es.iestriana.poo02.datos.Punto;
+import es.iestriana.poo02.datos.Triangulo;
 
 public class Principal {
 
@@ -14,6 +15,11 @@ public class Principal {
 		System.out.println("Area: " + c.calcularArea());
 		System.out.println("Perimetro: " + c.calcularPerimetro());
 		System.out.println("La distancaia del centro a b es: " + c.calcularDistanciaDesde(b));
+		
+		Triangulo t = new Triangulo(a, b, new Punto(4, 5));
+		System.out.println("Area: " + t.calcularArea());
+		System.out.println("Perimetro: " + t.calcularPerimetro());
+		System.out.println("La distancia de p1 a b es: " + t.calcularDistanciaDesde(b));
 	} 
 
 }
