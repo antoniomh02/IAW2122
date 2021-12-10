9dfce8b82f1a4e6236c405920b2e063929284c7b
antoniomh02 <a_antmayhur@iestriana.com>
2021-12-10 18:24:53 +0100

    poo03

---
+++ POO03/src/es/iestriana/poo/datos/Asignatura.java
@@ -0,0 +1,36 @@
+package es.iestriana.poo.datos;
+
+public class Asignatura {
+	
+	private int identificador;
+	private double calificacion;
+	
+	
+	public Asignatura(int identificador) {
+		super();
+		this.identificador = identificador;
+	}
+
+
+	public double getCalificacion() {
+		return calificacion;
+	}
+
+
+	public void setCalificacion(double calificacion) {
+		this.calificacion = calificacion;
+	}
+
+
+	public int getIdentificador() {
+		return identificador;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Asignatura [identificador=" + identificador + ", calificacion=" + calificacion + "]";
+	}
+	
+	
+}
